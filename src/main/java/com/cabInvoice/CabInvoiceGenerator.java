5a7929d2c98c8a01691128c387f82117ad83b291
drisyaathul <drisyak809@gmail.com>
2023-05-24 20:33:24 +0800

    Added: Calculate Total Fare

--- src/main/java/com/cabInvoice/CabInvoiceGenerator.java
+++ src/main/java/com/cabInvoice/CabInvoiceGenerator.java
@@ -1,5 +1,23 @@
 package com.cabInvoice;
 
 public class CabInvoiceGenerator {
+    /*
+     * UC1:
+     * Given Cost per kilometer, Cost per Minute and Minimum Fare.
+     */
+    public static final double costPerKilometer = 10.0;
+    public static final double costPerMinute = 1.0;
+    public static final double minimumFare = 5;
 
+    public double calculateFare(double distance, int time) {
+        /*
+         * Calculating Total Fare.
+         * If total fare is less than 5,then return minimum fare. otherwise return total fare itself.
+         */
+        double totalFare = (distance * costPerKilometer) + (time * costPerMinute);
+        if (totalFare < 5) {
+            return minimumFare;
+        }
+        return totalFare;
+    }
 }
--- src/test/java/com/cabInvoice/CabInvoiceGeneratorTest.java
+++ src/test/java/com/cabInvoice/CabInvoiceGeneratorTest.java
@@ -1,5 +1,23 @@
 package com.cabInvoice;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class CabInvoiceGeneratorTest {
+    /*
+     * creating object of CabInvoiceGenerator.
+     */
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+    @Test
+    public void givenDistanceShouldReturnFare(){
+        double distance = 2.0;
+        int time = 5;
+        double result = cabInvoiceGenerator.calculateFare(distance,time);
+        Assertions.assertEquals(25,result);
+    }
+
+
+
+
 
 }
