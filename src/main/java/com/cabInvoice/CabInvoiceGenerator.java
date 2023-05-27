6041333ba145b48365c73d5ed219254e71a4aa5a
drisyaathul <drisyak809@gmail.com>
2023-05-27 16:01:57 +0800

    Added: Premium Rides

--- src/main/java/com/cabInvoice/CabInvoiceGenerator.java
+++ src/main/java/com/cabInvoice/CabInvoiceGenerator.java
@@ -1,18 +1,19 @@
 package com.cabInvoice;
 
 public class CabInvoiceGenerator {
-    /*
-     * UC1:
-     * Given Cost per kilometer, Cost per Minute and Minimum Fare.
-     */
+
     public static final double costPerKilometer = 10.0;
     public static final double costPerMinute = 1.0;
     public static final double minimumFare = 5;
+    public static final double costPerKilometer_Premium = 15.0;
+    public static final double costPerMinute_Premium = 2.0;
+    public static final double minimumFare_Premium = 20;
 
     public double calculateFare(double distance, int time) {
         /*
          * Calculating Total Fare.
          * If total fare is less than 5,then return minimum fare. otherwise return total fare itself.
+         * If total fare is less than 20,then return minimum fare_Premium. otherwise return total fare_Premium itself.
          */
         double totalFare = (distance * costPerKilometer) + (time * costPerMinute);
         if (totalFare < 5) {
@@ -20,14 +21,31 @@ public class CabInvoiceGenerator {
         }
         return totalFare;
     }
+    public double calculateFarePremium(double distance, int time) {
+        /*
+         * Calculating Total Fare.
+         * If total fare is less than 20,then return minimum fare_Premium. otherwise return total fare itself.
+         */
+        double totalFare = (distance * costPerKilometer_Premium) + (time * costPerMinute_Premium);
+        if (totalFare < 20) {
+            return minimumFare;
+        }
+        return totalFare;
+    }
     //UC2:
     public double calculateAggregateFare(Ride[] rides){
-        /*
 
-         */
         double aggregateFare = 0;
         for (Ride ride : rides) {
             aggregateFare += calculateFare(ride.getDistance(),ride.getTime());
+        }
+        return aggregateFare;
+    }
+    public double calculateAggregateFarePremium(Ride[] rides){
+
+        double aggregateFare = 0;
+        for (Ride ride : rides) {
+            aggregateFare += calculateFarePremium(ride.getDistance(),ride.getTime());
         }
         return aggregateFare;
     }
@@ -43,6 +61,18 @@ public class CabInvoiceGenerator {
         double averageFarePerRide = (totalFare/totalNumberOfRides);
 
         return new Invoice(totalNumberOfRides,totalFare,averageFarePerRide);
+    }
+    public Invoice invoiceGeneratorPremium(Ride[] rides){
+        /*
+         * Return Total Number of Rides
+         * Return the Total Fare
+         * Return Average Fare per Ride
+         */
+        int totalNumberOfRides = rides.length;
+        double totalFarePremium = calculateAggregateFarePremium(rides);
+        double averageFarePerRidePremium = (totalFarePremium/totalNumberOfRides);
+
+        return new Invoice(totalNumberOfRides,totalFarePremium,averageFarePerRidePremium);
     }
 
 }
